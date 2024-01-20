5d145f601ce1fd59e521f87366967d02df54fb57
Chris Smart <Chris@Chriss-MacBook-Pro.local>
2024-01-20 19:42:58 +0000

    cannot get dayJS to work properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,24 @@ var dayOfMonth = dayjs().format("DD");
 currentDayEl.append(`${dayOfWeek}, ${dayOfMonth}th ${month}`).css("color", "#06aed5");
 
 // Color-code each timeblock based on past, present, and future
+var currentHour = dayjs().format("hA");
+
+$(".time-block").each(function (index, element) {
+  var blockTime = dayjs($(element).data("time"), "hA");
+
+  // Compare current time with the block time
+  if (blockTime.isBefore(dayjs(currentHour))) {
+    // Past
+    $(element).addClass("past");
+  } else if (blockTime.isSame(dayjs(currentHour))) {
+    // Present
+    $(element).addClass("present");
+  } else {
+    // Future
+    $(element).addClass("future");
+  }
+});
+
 // Allow a user to enter an event when they click a timeblock
 // Save the event in local storage when the save button is clicked in that timeblock.
 // Persist events between refreshes of a page
