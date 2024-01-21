b48ea588973bdf3c8311abbf5aa9bed0583bc7a4
Chris Smart <Chris@Chriss-MacBook-Pro.local>
2024-01-21 07:55:49 +0000

    managed to colour code time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,23 +8,48 @@ var dayOfMonth = dayjs().format("DD");
 currentDayEl.append(`${dayOfWeek}, ${dayOfMonth}th ${month}`).css("color", "#06aed5");
 
 // Color-code each timeblock based on past, present, and future
-var currentHour = dayjs().format("hA");
+function colourCodeTimeBlock() {
+  // get current hour
+  var currentHour = dayjs().format("H");
+  console.log(currentHour);
 
-$(".time-block").each(function (index, element) {
-  var blockTime = dayjs($(element).data("time"), "hA");
+  // loop through blocks
+  $(".time-block").each(function (index, element) {
+    var hourBlock = $(element).attr("id").split("hr"[1]);
+    // console.log(hourBlock[1]);
+    // switch classes based on past, present and future
+    if (hourBlock < currentHour) {
+      $(element).removeClass("present");
+      $(element).removeClass("future");
+      $(element).addClass("past");
+    } else if (hourBlock === currentHour) {
+      $(element).removeClass("past");
+      $(element).removeClass("future");
+      $(element).addClass("present");
+    } else {
+      $(element).removeClass("past");
+      $(element).removeClass("past");
+      $(element).addClass("future");
+    }
+  });
+}
+colourCodeTimeBlock();
+// $(".time-block").each(function (index, element) {
+//   var blockTime = dayjs($(element).data("time"), "hA");
+//   console.log(blockTime);
 
-  // Compare current time with the block time
-  if (blockTime.isBefore(dayjs(currentHour))) {
-    // Past
-    $(element).addClass("past");
-  } else if (blockTime.isSame(dayjs(currentHour))) {
-    // Present
-    $(element).addClass("present");
-  } else {
-    // Future
-    $(element).addClass("future");
-  }
-});
+//   // Compare current time with the block time
+//   if (blockTime.isBefore(dayjs(currentHour))) {
+//     // Past
+//     $(element).addClass("past");
+//   } else if (blockTime.isSame(dayjs(currentHour))) {
+//     // Present
+//     $(element).addClass("present");
+//   } else {
+//     // Future
+//     $(element).addClass("future");
+//   }
+// });
 
 // Allow a user to enter an event when they click a timeblock
 // Save the event in local storage when the save button is clicked in that timeblock.
