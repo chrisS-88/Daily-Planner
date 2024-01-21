6a8445b0ae6fa5fbfc8f1ca9ca9c9ec4d8361605
Chris Smart <Chris@Chriss-MacBook-Pro.local>
2024-01-21 18:28:00 +0000

    save event in local storage seems to be working

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,12 +11,11 @@ currentDayEl.append(`${dayOfWeek}, ${dayOfMonth}th ${month}`).css("color", "#06a
 function colourCodeTimeBlock() {
   // get current hour
   var currentHour = dayjs().format("H");
-  console.log(currentHour);
 
   // loop through blocks
   $(".time-block").each(function (index, element) {
-    var hourBlock = $(element).attr("id").split("hr"[1]);
-    // console.log(hourBlock[1]);
+    var hourBlock = parseInt($(element).attr("id").split("hr")[1]);
+
     // switch classes based on past, present and future
     if (hourBlock < currentHour) {
       $(element).removeClass("present");
@@ -33,24 +32,12 @@ function colourCodeTimeBlock() {
     }
   });
 }
-colourCodeTimeBlock();
-// $(".time-block").each(function (index, element) {
-//   var blockTime = dayjs($(element).data("time"), "hA");
-//   console.log(blockTime);
-
-//   // Compare current time with the block time
-//   if (blockTime.isBefore(dayjs(currentHour))) {
-//     // Past
-//     $(element).addClass("past");
-//   } else if (blockTime.isSame(dayjs(currentHour))) {
-//     // Present
-//     $(element).addClass("present");
-//   } else {
-//     // Future
-//     $(element).addClass("future");
-//   }
-// });
 
 // Allow a user to enter an event when they click a timeblock
+
 // Save the event in local storage when the save button is clicked in that timeblock.
+$(".saveBtn").on("click", function (event) {
+  var input = $(this).siblings(".description").val();
+  localStorage.setItem("User input", input);
+});
 // Persist events between refreshes of a page
