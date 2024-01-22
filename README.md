606bd9a196c867a779d3d452bdd1e15f4fdeabe9
Chris Smart <Chris@Chriss-MacBook-Pro.local>
2024-01-22 17:00:37 +0000

    readme finished and got date ordinal to work

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 
 ## Description
 
-A single day work schedule to help plan out those busy days. The project provides the current date along with work day blocks of time in increments of 1 hour.
+A single day work schedule to help plan out those busy days. The project provides the current date along with work day time blocks with 1 hour increments.
 The user can add events to the schedule and save the input via the save button provided at each block. Time blocks are colour coded based on
-the past, present and future hour in any given day. Events are stored locally to when saved to prevent them from dissapearing on refresh.
+the past, present and future hour in any given day. Events are stored locally when saved to prevent them from dissapearing on refresh.
 
-## Deployed app
+[Deployed app](https://chriss-88.github.io/Daily-Planner/)
+
+## Screenshot
+
+<img src="assets/imgs/app_screenshot.png" height = 400px>
 
 ## Credits
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,66 @@
-// trying to add advanced formats for date ordinal
-// dayjs.extend(window.dayjs_plugin_advancedFormat);
+$(document).ready(function () {
+  // display current date
+  const currentDayEl = $("#currentDay");
 
-// display current date
-const currentDayEl = $("#currentDay");
-// 'DO' NOT WORKING
-currentDayEl.append(dayjs().format("dddd, D MMMM")).css({
-  color: "rgb(99, 136, 137)",
-  paddingTop: "30px",
-  fontSize: "28px",
-});
-
-// Color-code each timeblock based on past, present, and future
-function colourCodeTimeBlock() {
-  // get current hour
-  var currentHour = dayjs().format("H");
-
-  // loop through blocks
-  $(".time-block").each(function (index, element) {
-    var hourBlock = parseInt($(element).attr("id").split("hr")[1]);
-
-    // switch classes based on past, present and future
-    if (hourBlock < currentHour) {
-      $(element).removeClass("present");
-      $(element).removeClass("future");
-      $(element).addClass("past");
-    } else if (hourBlock == currentHour) {
-      $(element).removeClass("past");
-      $(element).removeClass("future");
-      $(element).addClass("present");
-    } else {
-      $(element).removeClass("past");
-      $(element).removeClass("past");
-      $(element).addClass("future");
-    }
+  currentDayEl.append(dayjs().format("dddd, Do MMMM")).css({
+    color: "rgb(99, 136, 137)",
+    paddingTop: "30px",
+    fontSize: "28px",
   });
-}
 
-// Allow a user to enter an event when they click a timeblock
-// Save the event in local storage when the save button is clicked in that timeblock.
-$(".saveBtn").on("click", function (event) {
-  // grab the input from description field for local storage
-  var input = $(this).siblings(".description").val();
+  // Color-code each timeblock based on past, present, and future
+  function colourCodeTimeBlock() {
+    // get current hour
+    var currentHour = dayjs().format("H");
 
-  // grab the time related to description field for local storage
-  var hour = $(this).parent().attr("id");
+    // loop through blocks
+    $(".time-block").each(function (index, element) {
+      var hourBlock = parseInt($(element).attr("id").split("hr")[1]);
 
-  // set both to local storage
-  localStorage.setItem(hour, input);
+      // switch classes based on past, present and future
+      if (hourBlock < currentHour) {
+        $(element).removeClass("present");
+        $(element).removeClass("future");
+        $(element).addClass("past");
+      } else if (hourBlock == currentHour) {
+        $(element).removeClass("past");
+        $(element).removeClass("future");
+        $(element).addClass("present");
+      } else {
+        $(element).removeClass("past");
+        $(element).removeClass("past");
+        $(element).addClass("future");
+      }
+    });
+  }
+
+  // Allow a user to enter an event when they click a timeblock
+  // Save the event in local storage when the save button is clicked in that timeblock.
+  $(".saveBtn").on("click", function (event) {
+    // grab the input from description field for local storage
+    var input = $(this).siblings(".description").val();
+
+    // grab the time related to description field for local storage
+    var hour = $(this).parent().attr("id");
+
+    // set both to local storage
+    localStorage.setItem(hour, input);
+  });
+
+  // Persist events between refreshes of a page for each block
+  function persistEvents() {
+    $("#hr9").children(".description").val(localStorage.getItem("hr9"));
+    $("#hr10").children(".description").val(localStorage.getItem("hr10"));
+    $("#hr11").children(".description").val(localStorage.getItem("hr11"));
+    $("#hr12").children(".description").val(localStorage.getItem("hr12"));
+    $("#hr13").children(".description").val(localStorage.getItem("hr13"));
+    $("#hr14").children(".description").val(localStorage.getItem("hr14"));
+    $("#hr15").children(".description").val(localStorage.getItem("hr15"));
+    $("#hr16").children(".description").val(localStorage.getItem("hr16"));
+    $("#hr17").children(".description").val(localStorage.getItem("hr17"));
+  }
+
+  // call functions
+  persistEvents();
+  colourCodeTimeBlock();
 });
-
-// Persist events between refreshes of a page for each block
-function persistEvents() {
-  $("#hr9").children(".description").val(localStorage.getItem("hr9"));
-  $("#hr10").children(".description").val(localStorage.getItem("hr10"));
-  $("#hr11").children(".description").val(localStorage.getItem("hr11"));
-  $("#hr12").children(".description").val(localStorage.getItem("hr12"));
-  $("#hr13").children(".description").val(localStorage.getItem("hr13"));
-  $("#hr14").children(".description").val(localStorage.getItem("hr14"));
-  $("#hr15").children(".description").val(localStorage.getItem("hr15"));
-  $("#hr16").children(".description").val(localStorage.getItem("hr16"));
-  $("#hr17").children(".description").val(localStorage.getItem("hr17"));
-}
-
-// call functions
-persistEvents();
-colourCodeTimeBlock();
